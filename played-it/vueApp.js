fc42a7027221c7cfb4c34eb45cef815a1a7b821f
Mike Eovino <meovino@redhat.com>
2018-08-27 01:13:51 -0400

    Add routing

--- played-it/vueApp.js
+++ played-it/vueApp.js
@@ -1,13 +1,37 @@
-const url = "https://api.github.com/repositories/137301268/contents/played-it/data";
+var countriesUrl = "https://api.github.com/repositories/137301268/contents/played-it/data/";
+
+const home = {
+  template: '<div style = "border-radius:20px;background-color:red;width:200px;height:50px;margin:10px;font-size:25px;padding:10px;">This is the home route</div>'
+}
+const Route1 = {
+  template: '<div style = "border-radius:20px;background-color:cyan;width:200px;height:50px;margin:10px;font-size:25px;padding:10px;">This is router 1</div>'
+}
+const Route2 = {
+  template: '<div style = "border-radius:20px;background-color:green;width:200px;height:50px;margin:10px;font-size:25px;padding:10px;">This is router 2</div>'
+}
+const routes = [
+   { path: '/home', component: home },
+   { path: '/route1', component: Route1 },
+   { path: '/route2', component: Route2 }
+];
+const router = new VueRouter({
+  routes // short for `routes: routes`
+});
 
 const vm = new Vue({
         el: '#app',
+        router,
         data: {
           results: []
         },
         mounted() {
-          axios.get(url).then(response => {
-            this.results = response.data
-          })
+            this.getData();
+        },
+        methods: {
+          getData() {
+            axios.get(countriesUrl).then(response => {
+              this.results = response.data
+            })
+          }
         }
       });
