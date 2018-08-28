575b9e7de4af152f307c37719d2c5c97cf7e8804
Mike Eovino <meovino@redhat.com>
2018-08-27 23:12:29 -0400

    Remove routing

--- played-it/vueApp.js
+++ played-it/vueApp.js
@@ -1,45 +1,23 @@
 var baseUrl = "https://api.github.com/repositories/137301268/contents/played-it/data/";
 
-const home = {
-  template: '<div style = "border-radius:20px;background-color:red;width:200px;height:50px;margin:10px;font-size:25px;padding:10px;">This is the home route</div>'
-}
-
-const Route1 = {
-  template: '<div style = "border-radius:20px;background-color:cyan;width:200px;height:50px;margin:10px;font-size:25px;padding:10px;">This is router 1</div>'
-}
-
-const Route2 = {
-  template: '<div style = "border-radius:20px;background-color:green;width:200px;height:50px;margin:10px;font-size:25px;padding:10px;">This is router 2</div>'
-}
-
-const country = {
-  template: '<div style = "border-radius:20px;background-color:purple;width:200px;height:50px;margin:10px;font-size:25px;padding:10px;">country template</div>'
-}
-
-const routes = [
-   { path: '/home', component: home },
-   { path: '/route1', component: Route1 },
-   { path: '/route2', component: Route2 },
-   { path: '/country/:country', component: country },
-];
-const router = new VueRouter({
-  routes // short for `routes: routes`
-});
-
 const vm = new Vue({
-        el: '#app',
-        router,
-        data: {
-          results: []
-        },
-        mounted() {
-            this.getData();
-        },
-        methods: {
-          getData() {
-            axios.get(baseUrl).then(response => {
-              this.results = response.data
-            })
-          }
-        }
-      });
+  el: '#app',
+  data: {
+    previousSuffix: "",
+    currentSuffix: "",
+    suffix: "",
+    results: [],
+    counter: 0
+  },
+  mounted() {
+      this.getData(this.suffix);
+  },
+  methods: {
+    getData: function(suffix) {
+      console.log('suffix=' + suffix);
+      var url = baseUrl + suffix;
+      console.log(url);
+      axios.get(url).then(response => { this.results = response.data });
+    }
+  }
+});
