ce97c0de419e0655126e90061ca8d76a1bb9b671
Mike Eovino <meovino@redhat.com>
2018-08-28 21:35:04 -0400

    stuff

--- played-it/vueApp.js
+++ played-it/vueApp.js
@@ -1,3 +1,4 @@
+var url = "";
 var baseUrl = "https://meovino.github.io/played-it/data/";
 // contents api - https://api.github.com/repos/meovino/meovino.github.io/contents
 // trees api - https://api.github.com/repos/meovino/meovino.github.io/git/trees/{sha}
@@ -17,9 +18,16 @@ const vm = new Vue({
   methods: {
     getData: function(suffix) {
       console.log('suffix=' + suffix);
-      var url = baseUrl + suffix;
+      if (url == "") {
+        url = baseUrl;
+        console.log('first time through');
+      } else {
+        url = url + suffix;
+        console.log('subsequent time through');
+      }
       console.log(url);
       axios.get(url).then(response => { this.results = response.data });
+
     }
   }
 });
