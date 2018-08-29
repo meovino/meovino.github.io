d75cb19bf8065d20aeda62587e8ef9b8677c76c4
Mike Eovino <meovino@redhat.com>
2018-08-28 21:04:50 -0400

    start using static index.json files

--- played-it/vueApp.js
+++ played-it/vueApp.js
@@ -1,11 +1,11 @@
-var baseUrl = "https://api.github.com/repositories/137301268/contents/played-it/data/";
+var baseUrl = "https://meovino.github.io/played-it/data/";
 // https://api.github.com/repos/meovino/played-it/git/trees/45cb41bb3ec39302ff40ecb199e166ae76b66d95
 // https://api.github.com/repos/meovino/meovino.github.io/git/trees/45cb41bb3ec39302ff40ecb199e166ae76b66d95
 
 const vm = new Vue({
   el: '#app',
   data: {
-    previousSuffix: "",   
+    previousSuffix: "",
     currentSuffix: "",
     suffix: "",
     results: [],
